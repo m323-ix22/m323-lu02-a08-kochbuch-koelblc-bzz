b0d2f1003b80f13b60d6fbda81d8af1dcd35d28b
celine <koelblc@bzz.ch>
2024-09-02 14:54:12 +0200

    added load_recipe and adjust_recipe functions

--- main.py
+++ main.py
@@ -1,6 +1,35 @@
-# Dein Code kommt hier hin
+import json
+
+
+def load_recipe(json_string):
+    """Wandelt einen JSON-kodierten String in ein Python-Dictionary um."""
+    return json.loads(json_string)
+
+
+def adjust_recipe(recipe, num_people):
+    """Passt die Mengenangaben eines Rezepts an die gegebene Anzahl an Personen an."""
+    original_servings = recipe['servings']
+    factor = num_people / original_servings
+
+    adjusted_ingredients = {
+        ingredient: int(amount * factor)
+        for ingredient, amount in recipe['ingredients'].items()
+    }
+
+    adjusted_recipe = {
+        'title': recipe['title'],
+        'ingredients': adjusted_ingredients,
+        'servings': num_people
+    }
+
+    return adjusted_recipe
+
 
 if __name__ == '__main__':
-    # Beispiel für die Datenstruktur eines Rezepts
     recipe_json = '{"title": "Spaghetti Bolognese", "ingredients": {"Spaghetti": 400, "Tomato Sauce": 300, "Minced Meat": 500}, "servings": 4}'
-    # Dein Code kommt hier hin
+
+    recipe = load_recipe(recipe_json)
+
+    adjusted_recipe = adjust_recipe(recipe, 2)
+
+    print(json.dumps(adjusted_recipe, indent=4))
